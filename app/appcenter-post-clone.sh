c30bd18cb4c21e04cdb1ecaceefb4babc0c070f4
Evgenii Khramkov <evkhramk@microsoft.com>
2019-07-01 13:56:41 -0700

    log

--- app/appcenter-post-clone.sh
+++ app/appcenter-post-clone.sh
@@ -2,3 +2,5 @@ ls -la "$HOME/.gradle/init.d"
 
 mv "$HOME/.gradle/init.d/app-center-plugin.gradle" "$HOME/.gradle/init.d/app-center-plugin.gradle_old"
 mv ./app-center-plugin.gradle "$HOME/.gradle/init.d/app-center-plugin.gradle"
+
+tail -n 60 "$HOME/.gradle/init.d/app-center-plugin.gradle"
